2fdb1f25999b870b4ba9f79da067c629943f9709
Munir Rajpoot <munirrajpoot1012@gmail.com>
2023-08-01 10:51:42 -0700

    Advance_JS

--- JS Complete/Async.js
+++ JS Complete/Async.js
@@ -2,6 +2,8 @@
 //Synchronous:Blocking Behaviour
 //Asynchronous:Non Blocking Behaviour
 
+const { log } = require("async");
+
 //CallBacks
 //  This is just like a function that passes as argument to another function
 
@@ -14,13 +16,33 @@
 // }
 // doSomething(sayHi);
 
-function greet(name){
-    console.log(`Hello! ${name("Muneer")}`);
+// function greet(name){
+//     console.log(`Hello! ${name("Muneer")}`);
+// }
+// function fullName(name){
+// return name;
+// }
+// greet(fullName);
+
+//Promises
+
+
+let prom=new Promise(function(resolved,rejected){
+let a=false;
+if(a===true){
+    resolved(a);
 }
-function fullName(name){
-return name;
-}
-greet(fullName);
+rejected(a);
+});
+prom.then(function(value){
+    console.log(`${value} is resolved.... `)
+},
+function(error){
+    console.log(`${error} is rejected`);
+},
+);
+
+
 
 
 
